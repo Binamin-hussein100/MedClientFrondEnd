e70249ff4c6314cd6ddf9186252f7ec2e966e31e
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2024-05-20 12:59:19 +0300

    Initial repo

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// export const plugins = [
+//     // eslint-disable-next-line no-undef
+//     require('tailwindcss'),
+//     // eslint-disable-next-line no-undef
+//     require('autoprefixer'),
+// ];
+  
