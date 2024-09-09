a222768546dd92566f833ecb54280c95df55cb3a
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2024-09-09 13:53:02 +0300

    Add: add each order component as route

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
-
-import './App.css'
+import './App.css';
 import Welcome from './components/welcome';
 import Navbar1 from './components/navbar';
-import {  Routes, Route } from 'react-router-dom';
+import { Routes, Route,useLocation } from 'react-router-dom';
 import SignUp from './components/subComponent/signup';
 import SignIn from './components/subComponent/signin';
 import NewOrder from './components/newOrder';
-
+import OrdersList from './components/ClientDashboard/ordersList';
+import EachOrder from './components/ClientDashboard/eachOrder';
 
 function App() {
+  const location = useLocation();
+
+  const showNavbar = location.pathname === '/' || location.pathname === '/signin' || location.pathname === '/signup'  || location.pathname === '/newOrder';
+
 
   return (
     <>
-      <Navbar1/>
-        <div className="">
-            
-            <Routes>
-                <Route path='/' element={<Welcome/>}/>
-                <Route path='/signup' element={<SignUp/>}/>
-                <Route path='/signin' element={<SignIn/>}/>
-                <Route path='/newOrder' element={<NewOrder/>}/>
-            </Routes>
-        </div> 
+      {showNavbar && <Navbar1 />}
+      <div className="">
+        <Routes>
+          <Route path='/' element={<Welcome />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/newOrder' element={<NewOrder />} />
+          <Route path='/orders' element={<OrdersList />} />
+          <Route path='/order/:id' element={<EachOrder/>} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
-{/* <div className=''>
-<Routes>
-    <Route path='/' element={<Welcome/>}/>
-    <Route path='orders' element={<OrdersList/>}/>
-    {/* <Route path='notification' element={<Notification/>}/> */}
-    {/* <Route path='notification'/> */}
-
-// </Routes>
-// <Footer/>
-// </div> */}
 
